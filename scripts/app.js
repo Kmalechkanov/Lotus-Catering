e52def8f9f61bf4e7a381790d822fcd3e6909ba9
Kaloyan Malechkanov <42181201+Kmalechkanov@users.noreply.github.com>
2020-02-03 15:26:52 +0200

    second main header with transition

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+let lastScroll = 0;
+
 $(window).scroll(function (e) {
   if ($(window).scrollTop() >= $(".logo-wrapper").height()) {
     $("header nav").addClass("fixed-header");
@@ -6,4 +8,16 @@ $(window).scroll(function (e) {
     $("header nav").removeClass("fixed-header");
     //$("nav div").removeClass("visible-title");
   }
+  
+  console.log($(window).scrollTop() + " " + $(".logo-wrapper").height());
+  if ($(window).scrollTop() <= lastScroll || $(window).scrollTop() <= $(".logo-wrapper").height()) {
+    $(".secondHeader").addClass("show-block");
+    $(".secondHeader").removeClass("hide-block");
+  } else {
+    $(".secondHeader").addClass("hide-block");
+    $(".secondHeader").removeClass("show-block");
+
+  } 
+
+  lastScroll = $(window).scrollTop();
 });
