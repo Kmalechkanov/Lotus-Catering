4b44f78a095fa2e17a5627ce32345d334fdadfac
Kaloyan Malechkanov <42181201+Kmalechkanov@users.noreply.github.com>
2020-02-12 09:43:04 +0200

    responsive main container

--- AnisMasterpieces/AnisMasterpieces/wwwroot/js/site.js
+++ AnisMasterpieces/AnisMasterpieces/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let lastScroll = 0;
 
-// Write your JavaScript code.
+$(window).scroll(function (e) {
+    if ($(window).scrollTop() >= $(".logo-wrapper").height()) {
+        $("header nav").addClass("fixed-header");
+        $(".container").css("padding-top", $(".fixed-header").height()+100);
+        //$("nav div").addClass("visible-title");
+    } else {
+        $("header nav").removeClass("fixed-header");
+        $(".container").css("padding-top", 100);
+        //$("nav div").removeClass("visible-title");
+    }
+
+    if ($(window).scrollTop() <= lastScroll || $(window).scrollTop() <= $(".logo-wrapper").height()) {
+        $(".secondHeader").addClass("show-block");
+        $(".secondHeader").removeClass("hide-block");
+    } else {
+        $(".secondHeader").addClass("hide-block");
+        $(".secondHeader").removeClass("show-block");
+
+    }
+
+    lastScroll = $(window).scrollTop();
+});
