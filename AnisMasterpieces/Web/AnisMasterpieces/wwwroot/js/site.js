9ee6c93219a5715680c6d9bcb754b3f75d272601
Kaloyan Malechkanov <42181201+Kmalechkanov@users.noreply.github.com>
2020-02-25 11:56:12 +0200

    Edit login form

--- AnisMasterpieces/Web/AnisMasterpieces/wwwroot/js/site.js
+++ AnisMasterpieces/Web/AnisMasterpieces/wwwroot/js/site.js
@@ -4,11 +4,9 @@ $(window).scroll(function (e) {
     if ($(window).scrollTop() >= $(".logo-wrapper").height()) {
         $("header nav").addClass("fixed-header");
         $(".container").css("padding-top", $(".fixed-header").height()+100);
-        //$("nav div").addClass("visible-title");
     } else {
         $("header nav").removeClass("fixed-header");
         $(".container").css("padding-top", 100);
-        //$("nav div").removeClass("visible-title");
     }
 
     if ($(window).scrollTop() <= lastScroll || $(window).scrollTop() <= $(".logo-wrapper").height()) {
