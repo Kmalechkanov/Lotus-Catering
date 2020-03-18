3421c26ec2818eca1d0815f7a1cfe91597b58463
Kaloyan Malechkanov <42181201+Kmalechkanov@users.noreply.github.com>
2020-03-18 13:54:35 +0200

    ItemCartAndCartEntities migration

--- AnisMasterpieces/Data/AnisMasterpieces.Data.Models/Cart.cs
+++ AnisMasterpieces/Data/AnisMasterpieces.Data.Models/Cart.cs
@@ -9,13 +9,13 @@
         public Cart()
         {
             this.Id = Guid.NewGuid().ToString();
-            this.CartItems = new HashSet<CartItems>();
+            this.CartItems = new HashSet<CartItem>();
         }
 
         public string Id { get; set; }
 
         public ApplicationUser User { get; set; }
 
-        public ICollection<CartItems> CartItems { get; set; }
+        public ICollection<CartItem> CartItems { get; set; }
     }
 }
